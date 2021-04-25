a3fc97c03e82720348632c3f0e9cf8e6d1560c61
jinwook-song <wlsdnr129@naver.com>
2021-04-25 15:05:10 +0900

    lecture 1. if with a Twist

--- main.go
+++ main.go
@@ -2,25 +2,15 @@ package main
 
 import "fmt"
 
-func superAdd(numbers ...int) int{
-	sumNum := 0
-	for _, number:= range numbers {
-		sumNum += number
-	}
-	return sumNum
-}
-
-func greatAdd(numbers ...int) int {
-	sumNum := 0
-	for i := 0; i < len(numbers); i++ {
-		sumNum += numbers[i]
-	}
-	return sumNum
+func canIDrink(age int) bool {
+	if koreanAge := age + 2; koreanAge < 19 {
+		return false
+	} 
+	return true
 }
 
 func main() {
-	sum1 := superAdd(1214,421,41,412,12)
-	sum2 := greatAdd(1,2,3,1231414,5,6)
-
-	fmt.Println(sum1, sum2)
+	fmt.Println(canIDrink(16))
+	fmt.Println(canIDrink(17))
+	fmt.Println(canIDrink(18))
 }
