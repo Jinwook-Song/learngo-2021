ee4989d611591bb8f076d91344f0dfee6153d281
jinwook-song <wlsdnr129@naver.com>
2021-04-25 15:39:16 +0900

    lecture 1. Maps

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	names := []string{"nico","lynn","jinwook"}
-	// array를 return하지 않으므로 값을 할당해야 함
-	names = append(names, "golang") 
-	fmt.Println(names)
+	jinwook := map[string]string{"name":"jw", "age":"29"}
+	fmt.Println(jinwook)
+
+	for key, value := range jinwook {
+		fmt.Println(key, value)
+	}
 }
