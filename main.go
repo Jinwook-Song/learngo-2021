516d34faa01b0a9ea032287b07fb428b88ae686e
jinwook-song <wlsdnr129@naver.com>
2021-04-26 13:22:05 +0900

    lecture 2. dictionary method

--- main.go
+++ main.go
@@ -3,19 +3,44 @@ package main
 import (
 	"fmt"
 
-	"github.com/jinwook-song/learngo-2021/accounts"
+	"github.com/jinwook-song/learngo-2021/mydict"
 )
 
 func main() {
-	accounts := accounts.NewAccount("nico")
-	accounts.Deposit(10)
-	fmt.Println(accounts.Balacne())
-	// error handle
-	err := accounts.Withdraw(12)
+	dictionary := mydict.Dictionary{"first":"First word"}
+
+	baseWord := "hello"
+
+	// Search
+	definition, err := dictionary.Search("first")
 	if err != nil {
 		fmt.Println(err)
+	} else {
+		fmt.Println(definition)
 	}
-	fmt.Println(accounts )
-	accounts.ChangeOwner("jinwook")
-	fmt.Println("changed owner:" ,accounts.Owner())
+
+	// Add none exist word
+	err2 := dictionary.Add(baseWord, "greeting")
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+	hello, _ := dictionary.Search(baseWord)
+	fmt.Println(hello)
+
+	// Add exist word -> error
+	err3 := dictionary.Add(baseWord, "greeting")
+	if err3 != nil {
+		fmt.Println(err3)
+	}
+
+	// Update word
+	err4 := dictionary.Update(baseWord, "edit")
+	if err4 != nil {
+		fmt.Println(err4)
+	}
+	fmt.Println(dictionary[baseWord])
+
+	// Delete word
+	dictionary.Delete(baseWord)
+	fmt.Println(dictionary)
 }
