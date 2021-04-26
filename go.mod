bd2f79f25a225212acbc71b32a9c1b4e4e077921
jinwook-song <wlsdnr129@naver.com>
2021-04-26 18:25:09 +0900

    lecture 4. job scrapper

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jinwook-song/learngo-2021
 
 go 1.16
+
+require github.com/PuerkitoBio/goquery v1.6.1 // indirect
--- main.go
+++ main.go
@@ -1,49 +1,126 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-type requestResult struct {
-	url string
-	status int
+type extractedJob struct {
+	id string
+	title string
+	location string
+	summary string
 }
+
+var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main() {
-	results := make(map[string]string)
-	channel := make(chan requestResult)
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.google.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"https://academy.nomadcoders.co/",
-	}
+	jobs := []extractedJob{}
+	totalPages := getPages()
 
-	for _, url := range urls {
-		go hitURL(url, channel)
+	for i :=0; i<totalPages; i++ {
+		extractedJobs := getPage(i)
+		// ... means contents in array
+		jobs = append(jobs, extractedJobs...)
 	}
+	writeJobs(jobs)
+	fmt.Println("Done, extracted: ", len(jobs))
+}
 
-	for i:=0; i<len(urls); i++ {
-		result := <- channel
-		results[result.url] = fmt.Sprint(result.status)
-	}
+// save jobs to csv file
+func writeJobs(jobs []extractedJob) {
+	file, err := os.Create("jobs.csv")
+	checkErr(err)
 
-	for url, status := range results {
-		fmt.Println(url, status)
+	w := csv.NewWriter(file)
+	defer w.Flush()
+
+	headers := []string{"Link","Title","Location","Summary"}
+
+	wErr := w.Write(headers)
+	checkErr(wErr)
+
+	for _, job := range jobs {
+		jobSlice := []string{"https://kr.indeed.com/viewjob?jk="+job.id, job.title, job.location, job.summary}
+		jwErr := w.Write(jobSlice)
+		checkErr(jwErr)
 	}
 }
 
-// send only
-func hitURL(url string, channel chan<- requestResult)  {
-	resp, err := http.Get(url)
-	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println("ERROR❌", url, err, resp.StatusCode)
-	} 
-	channel <- requestResult{url: url, status: resp.StatusCode}
+func getPage(page int) []extractedJob{
+	// 한 페이지에 해당하는 jobs array
+	jobs := []extractedJob{}
+	pageURL := baseURL + "&start=" + fmt.Sprint(page*50)
+	fmt.Println(pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".jobsearch-SerpJobCard")
+
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		job := extractJob(card)
+		jobs = append(jobs, job)
+	})
+	return jobs
+} 
+
+func extractJob (card *goquery.Selection) extractedJob {
+	id, _ := card.Attr("data-jk")
+	title := cleanString(card.Find(".title>a").Text())
+	location := cleanString(card.Find(".sjcl").Text())
+	summary := cleanString(card.Find(".summary").Text())
+	return extractedJob{
+		id:id,
+		title:title,
+		location:location,
+		summary:summary}
+	
+}
+
+func getPages() int {
+	pages := 0
+	res, err := http.Get(baseURL)
+	checkErr(err)
+	checkCode(res)
+
+	// 메모리 누수 발생
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+
+		pages = s.Find("a").Length()
+	})
+
+	return pages
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Request failed with Status: ", res.StatusCode)
+	}
+}
+
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
 }
